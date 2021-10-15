1b60cc80b1bdce47b7b8bc5ed890a9067029fcc7
cristina-rc <91025823+cristina-rc@users.noreply.github.com>
2021-10-15 19:35:27 +0200

    Create 01.php

---
+++ Tema2/Ejercicios1/01.php
@@ -0,0 +1,15 @@
+<?php
+$num1 = random_int(1, 10);
+$num2 = random_int(1, 10);
+
+echo $num1, "</br>";
+echo $num2, "</br>";
+
+echo ($num1+$num2) . "</br>";
+echo ($num1-$num2) . "</br>";
+echo ($num1*$num2) . "</br>";
+echo ($num1/$num2) . "</br>";
+echo ($num1%$num2) . "</br>";
+echo ($num1**$num2) . "</br>";
+
+?>
