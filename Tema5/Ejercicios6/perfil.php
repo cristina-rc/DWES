0dc37803b3098c9244b0b5ca71fc8a2fd66d2323
cristina-rc <91025823+cristina-rc@users.noreply.github.com>
2021-11-04 09:02:49 +0100

    Update perfil.php

--- Tema5/Ejercicios6/perfil.php
+++ Tema5/Ejercicios6/perfil.php
@@ -16,9 +16,10 @@
 
         $deportes = $_POST["deportes"];
 
-        foreach($deportes as $deporte){
-            setcookie($deporte, $deporte, time() + 7*24*3600);
-        }
+        $deportesString = implode(" ", $deportes);
+
+        setcookie("deportes", $deportesString, time() + 7*24*3600);
+
     } 
 
     if (isset($_POST["borrar"])) {
@@ -29,10 +30,6 @@
         header("Refresh:0"); 
     }
 
-    var_dump($_COOKIE["Futbol"]);
-    if(isset($_COOKIE["Futbol"])){
-        echo "existe";
-    };
 ?>
 
 
@@ -46,14 +43,14 @@
     
     <form action="#" method="POST">
         Edad:  <input type="number" name ="edad" value="<?=isset($edad)?$edad:""?>"><br>
-        <input type="radio" name="sexo" value="mujer" <?php if ($_COOKIE["sexo"] == "mujer"){echo 'checked';}?>>Mujer
-        <input type="radio" name="sexo" value="hombre" <?php if ($_COOKIE["sexo"] == "hombre"){echo 'checked';}?>>Hombre<br>
+        <input type="radio" name="sexo" value="mujer" <?php if ($sexo == "mujer"){echo 'checked';}?>>Mujer
+        <input type="radio" name="sexo" value="hombre" <?php if ($sexo == "hombre"){echo 'checked';}?>>Hombre<br>
         Deportes: 
         <select name = "deportes[]" multiple size=4>
-            <option <?php if (isset($_COOKIE["Futbol"])){echo 'selected="selected"';}?>>Futbol</option>
-            <option <?php if (isset($_COOKIE["Tenis"])){echo 'selected="selected"';}?>>Tenis</option>
-            <option <?php if (isset($_COOKIE["Ciclismo"])){echo 'selected="selected"';}?>>Ciclismo</option>
-            <option <?php if (isset($_COOKIE["Otro"])){echo 'selected="selected"';}?>>Otro</option>
+            <option <?php if (stripos($deportesString, 'Futbol')!==false){echo 'selected="selected"';}?>>Futbol</option>
+            <option <?php if (stripos($deportesString, 'Tenis')!==false){echo 'selected="selected"';}?>>Tenis</option>
+            <option <?php if (stripos($deportesString, 'Ciclismo')!==false){echo 'selected="selected"';}?>>Ciclismo</option>
+            <option <?php if (stripos($deportesString, 'Otro')!==false){echo 'selected="selected"';}?>>Otro</option>
         </select><br>
 
       <input type="submit" name="almacenar" value="Almacenar cookie">
