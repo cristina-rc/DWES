444a1d6fa5596c86a20ef352aa08612998152904
cristina-rc <91025823+cristina-rc@users.noreply.github.com>
2021-12-14 18:39:13 +0100

    Delete Tema8/EntregaCRUDProductos directory

--- Tema8/EntregaCRUDProductos/web/js/funciones.js
+++
@@ -1,9 +0,0 @@
-/**
- * Funciones auxiliares de javascripts 
- */
-function confirmarBorrar(descripcion,numero){
-  if (confirm("¿Quieres eliminar el producto:  "+descripcion+"?"))
-  {
-   document.location.href="?orden=Borrar&id="+numero;
-  }
-}
