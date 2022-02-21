ccb45b03c84d2edf91226d9ac676948bfa0bbf59
cristina-rc <91025823+cristina-rc@users.noreply.github.com>
2022-02-21 13:29:34 +0100

    Add files via upload

---
+++ Tema11/clientesplus/src/main/java/modelo/Cliente.java
@@ -0,0 +1,25 @@
+package modelo;
+
+public class Cliente {
+	  public int getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getPuntos() {
+		return puntos;
+	}
+	public void setPuntos(int puntos) {
+		this.puntos = puntos;
+	}
+	private int telefono;
+	  private String nombre;
+	  private int puntos;	  
+}
