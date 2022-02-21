3d71991cefc9768915e60e37030e63ea3bc953f5
cristina-rc <91025823+cristina-rc@users.noreply.github.com>
2022-02-21 13:40:36 +0100

    Delete ProductosMVC directory

--- ProductosMVC/src/main/webapp/web/js/funciones.js
+++
@@ -1,9 +0,0 @@
-/**
- * Funciones auxiliares de javascripts 
- */
-function confirmarBorrar(id, nombre){
-  if (confirm("¿Quieres eliminar el producto:  "+nombre+"?"))
-  {
-   document.location.href="?orden=Borrar&producto_no="+id;
-  }
-}
